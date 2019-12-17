fc0142fc1009a9b883b43f00aadbc36529d532c7
toyobayashi <lifenglin314@outlook.com>
2019-12-17 22:56:12 +0800

    使用 ty 构建

---
+++ babel.config.js
@@ -0,0 +1,20 @@
+const commonPlugins = [
+  '@babel/plugin-proposal-object-rest-spread',
+  ['import', {
+    libraryName: 'antd',
+    libraryDirectory: 'es',
+    style: 'css' // `style: true` 会加载 less 文件
+  }]
+]
+
+module.exports = {
+  presets: [
+    '@babel/preset-react',
+    [
+      '@babel/preset-env', {
+        modules: false // import export 由 webpack 处理，不由 babel 处理
+      }
+    ]
+  ],
+  plugins: process.env.NODE_EMV !== 'production' ? ['react-hot-loader/babel', ...commonPlugins] : [...commonPlugins]
+}
---
+++ config/tyconfig.js
@@ -0,0 +1,26 @@
+const path = require('path')
+
+module.exports = {
+  entry: {
+    web: {
+      // 键名是打包后的js名字，入口文件，可以是多个入口，所以是个数组
+      bundle: [path.join(__dirname, '../src/index.jsx')]
+    }
+  },
+  output: {
+    // web 是指 web 构建目标
+    web: path.join(__dirname, '../dist')
+  },
+  contentBase: path.join(__dirname, '../dist'),
+  devServerHost: 'localhost',
+  devServerPort: 3000,
+  proxy: {
+    '/api': {
+      target: 'http://localhost',
+      changeOrigin: true,
+      pathRewrite: {
+        '^/api': '/' 
+      }
+    }
+  }
+}
