b039ac3a9f7b332f6431b53ed5438275c1b7b768
wormhole <363408268@qq.com>
2019-12-28 17:44:53 +0800

    按需加载

--- src/index.jsx
+++ src/index.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import 'antd/dist/antd.css';
 import './index.less';
 import {Provider} from 'react-redux';
 import store from './module/store';
