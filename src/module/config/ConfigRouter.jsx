b5fa9262ea98b2b15ed0ec928147b07b63293e32
wormhole <363408268@qq.com>
2020-01-20 16:49:19 +0800

    相对路径

--- src/module/config/ConfigRouter.jsx
+++ src/module/config/ConfigRouter.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import {Route} from "react-router-dom";
-import Config from "../config";
+import Config from "./index";
 
 const ConfigRouter = () => (
     <div>
--- src/module/personal/PersonalRouter.jsx
+++ src/module/personal/PersonalRouter.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import {Route} from "react-router-dom";
-import Personal from "../personal";
+import Personal from "./index";
 
 const PersonalRouter = () => (
     <div>
