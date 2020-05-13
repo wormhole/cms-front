94b99307b872e6e6d9a83a0c5cbfdc75ee805737
wormhole <363408268@qq.com>
2020-05-13 21:24:07 +0800

    国际化

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import {HashRouter, Route, Switch} from "react-router-dom";
+import zhCN from "antd/es/locale/zh_CN";
+import {ConfigProvider} from "antd";
 import {Provider} from "react-redux";
 import store from "./store";
 import Login from "./page/login";
@@ -10,11 +12,13 @@ import "./app.less";
 
 ReactDOM.render(
     <Provider store={store}>
-        <HashRouter>
-            <Switch>
-                <Route exact path="/login" component={Login}/>
-                <Route exact path="/register" component={Register}/>
-                <Route path="/" component={Home}/>
-            </Switch>
-        </HashRouter>
+        <ConfigProvider locale={zhCN}>
+            <HashRouter>
+                <Switch>
+                    <Route exact path="/login" component={Login}/>
+                    <Route exact path="/register" component={Register}/>
+                    <Route path="/" component={Home}/>
+                </Switch>
+            </HashRouter>
+        </ConfigProvider>
     </Provider>, document.getElementById("app"));
