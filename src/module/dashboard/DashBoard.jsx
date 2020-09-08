8444c07a795cee2c441a8748798cb97470a53523
wormhole <363408268@qq.com>
2020-09-08 21:23:33 +0800

    首页

--- src/module/dashboard/DashBoard.jsx
+++ src/module/dashboard/DashBoard.jsx
@@ -1,6 +1,6 @@
 import React, {Component} from "react";
 import {Avatar, Card, Col, message, Row} from "antd";
-import {Bar, Pie} from "@antv/g2plot";
+import {Bar, Donut} from "@ant-design/charts";
 import {FileOutlined, IdcardOutlined, MenuOutlined, TeamOutlined} from "@ant-design/icons";
 import axios from "../../util/axios";
 import {Link} from "react-router-dom";
@@ -13,70 +13,9 @@ class DashBoard extends Component {
 
     componentDidMount() {
         this.loadCount();
-        this.loadUserStatus(this.initUserStatusChart);
-        this.loadDiskInfo(this.initDiskChart);
-        this.loadMemInfo(this.initMemChart);
-    }
-
-    initUserStatusChart(userStatus) {
-        let data = [];
-        data.push({状态: "总数", 数量: userStatus.total});
-        data.push({状态: "启用", 数量: userStatus.enable});
-        data.push({状态: "在线", 数量: userStatus.online});
-        data.push({状态: "禁用", 数量: userStatus.disable});
-        data.push({状态: "锁定", 数量: userStatus.lock});
-
-        const barPlot = new Bar(document.getElementById("user"), {
-            forceFit: true,
-            barSize: 30,
-            data: data,
-            xField: "数量",
-            yField: "状态",
-            label: {
-                visible: true,
-                formatter: (v) => v + "人",
-            }
-        });
-
-        barPlot.render();
-    }
-
-    initDiskChart(diskInfo) {
-        let data = [];
-        data.push({type: "已使用", value: diskInfo.used});
-        data.push({type: "未使用", value: diskInfo.free});
-
-        let diskPlot = new Pie(document.getElementById("disk"), {
-            forceFit: true,
-            radius: 0.8,
-            data: data,
-            angleField: "value",
-            colorField: "type",
-            label: {
-                visible: true,
-                type: "spider",
-            },
-        });
-        diskPlot.render();
-    }
-
-    initMemChart(memInfo) {
-        let data = [];
-        data.push({type: "已使用", value: memInfo.used});
-        data.push({type: "未使用", value: memInfo.free});
-
-        let memPlot = new Pie(document.getElementById("mem"), {
-            forceFit: true,
-            radius: 0.8,
-            data: data,
-            angleField: "value",
-            colorField: "type",
-            label: {
-                visible: true,
-                type: "spider",
-            },
-        });
-        memPlot.render();
+        this.loadUserStatus();
+        this.loadDiskInfo();
+        this.loadMemInfo();
     }
 
     loadCount() {
@@ -91,10 +30,10 @@ class DashBoard extends Component {
         });
     }
 
-    loadUserStatus(callback) {
+    loadUserStatus() {
         axios.get(api.userStatus).then(result => {
             if (result.status) {
-                callback(result.data);
+                this.props.save({status: result.data});
             } else {
                 message.error(result.message);
             }
@@ -103,10 +42,10 @@ class DashBoard extends Component {
         });
     }
 
-    loadDiskInfo(callback) {
+    loadDiskInfo() {
         axios.get(api.diskInfo).then(result => {
             if (result.status) {
-                callback(result.data);
+                this.props.save({disk: result.data});
             } else {
                 message.error(result.message);
             }
@@ -115,10 +54,10 @@ class DashBoard extends Component {
         });
     }
 
-    loadMemInfo(callback) {
+    loadMemInfo() {
         axios.get(api.memInfo).then(result => {
             if (result.status) {
-                callback(result.data);
+                this.props.save({mem: result.data});
             } else {
                 message.error(result.message);
             }
@@ -128,6 +67,94 @@ class DashBoard extends Component {
     }
 
     render() {
+        let status = [], disk = [], mem = [];
+        status.push({数量: this.props.dashboard.status.total, 状态: "总数"});
+        status.push({数量: this.props.dashboard.status.enable, 状态: "启用"});
+        status.push({数量: this.props.dashboard.status.online, 状态: "在线"});
+        status.push({数量: this.props.dashboard.status.disable, 状态: "禁用"});
+        status.push({数量: this.props.dashboard.status.lock, 状态: "锁定"});
+
+        disk.push({type: "已使用", value: this.props.dashboard.disk.used});
+        disk.push({type: "未使用", value: this.props.dashboard.disk.free});
+
+        mem.push({type: "已使用", value: this.props.dashboard.mem.used});
+        mem.push({type: "未使用", value: this.props.dashboard.mem.free});
+
+        const statusConfig = {
+            title: {
+                visible: true,
+                text: "用户状态",
+            },
+            description: {
+                visible: true,
+                text: "不同状态下的用户数量统计",
+            },
+            forceFit: true,
+            data: status,
+            xField: "数量",
+            yField: "状态",
+            colorField: "状态",
+            color: ["#427cfb"],
+            label: {
+                visible: true,
+                position: "middle",
+                formatter: (v) => v + "人",
+            },
+        };
+
+        const diskConfig = {
+            legend: {
+                visible: true,
+                position: "bottom",
+            },
+            forceFit: true,
+            title: {
+                visible: true,
+                text: "磁盘使用量",
+            },
+            description: {
+                visible: true,
+                text:
+                    "磁盘使用百分比统计详情",
+            },
+            radius: 1,
+            padding: "auto",
+            data: disk,
+            angleField: "value",
+            colorField: "type",
+            label: {
+                visible: true,
+                type: "inner",
+                formatter: (v) => v + " GB",
+            },
+        };
+
+        const memConfig = {
+            legend: {
+                visible: true,
+                position: "bottom",
+            },
+            forceFit: true,
+            title: {
+                visible: true,
+                text: "内存使用量",
+            },
+            description: {
+                visible: true,
+                text:
+                    "内存使用百分比统计详情",
+            },
+            radius: 1,
+            padding: "auto",
+            data: mem,
+            angleField: "value",
+            colorField: "type",
+            label: {
+                visible: true,
+                type: "inner",
+                formatter: (v) => v + " GB",
+            },
+        };
         return (
             <div className="cms-module">
                 <div className="cms-module-dashboard">
@@ -176,17 +203,23 @@ class DashBoard extends Component {
                     <Row gutter={32} className="cms-module-row">
                         <Col span={8}>
                             <Card title="用户状态统计" className="cms-module-card">
-                                <div id="user" className="cms-module-chart"/>
+                                <div id="user" className="cms-module-chart">
+                                    <Bar {...statusConfig}/>
+                                </div>
                             </Card>
                         </Col>
                         <Col span={8}>
-                            <Card title="磁盘监控(单位：GB)" className="cms-module-card">
-                                <div id="disk" className="cms-module-chart"/>
+                            <Card title="磁盘监控" className="cms-module-card">
+                                <div id="disk" className="cms-module-chart">
+                                    <Donut  {...diskConfig}/>
+                                </div>
                             </Card>
                         </Col>
                         <Col span={8}>
-                            <Card title="内存监控(单位：GB)" className="cms-module-card">
-                                <div id="mem" className="cms-module-chart"/>
+                            <Card title="内存监控" className="cms-module-card">
+                                <div id="mem" className="cms-module-chart">
+                                    <Donut  {...memConfig}/>
+                                </div>
                             </Card>
                         </Col>
                     </Row>
--- src/module/dashboard/reducer.js
+++ src/module/dashboard/reducer.js
@@ -4,6 +4,23 @@ const initState = {
         menu: 0,
         user: 0,
         role: 0
+    },
+    disk: {
+        total: 0,
+        free: 0,
+        used: 0
+    },
+    mem: {
+        total: 0,
+        free: 0,
+        used: 0
+    },
+    status: {
+        online: 0,
+        lock: 0,
+        enable: 0,
+        disable: 0,
+        total: 0
     }
 };
 
